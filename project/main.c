fa867aa1f71278d0f7ec7704891295ea1698a4ab
Erich Styger <erich.styger@hslu.ch>
2023-07-26 15:10:53 +0200

    added project

---
+++ project/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ * Author: Erich Styger
+ * License: PDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello world!\n");
+  return 0;
+}
+
+
