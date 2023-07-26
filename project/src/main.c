e03e0ffcb6926977c4ecdd1c399ae2bd6f50692b
Erich Styger <erich.styger@hslu.ch>
2023-07-26 18:44:25 +0200

    cosmetic

--- project/src/main.c
+++ project/src/main.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-int main() {
+int main(main) {
+  const uint led_pin = 25;
 
-    const uint led_pin = 25;
+  // Initialize LED pin
+  gpio_init(led_pin);
+  gpio_set_dir(led_pin, GPIO_OUT);
 
-    // Initialize LED pin
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
+  // Initialize chosen serial port
+  stdio_init_all();
 
-    // Initialize chosen serial port
-    stdio_init_all();
-
-    // Loop forever
-    while (true) {
-
-        // Blink LED
-        printf("Blinking!\r\n");
-        gpio_put(led_pin, true);
-        sleep_ms(1000);
-        gpio_put(led_pin, false);
-        sleep_ms(1000);
-    }
+  // Loop forever
+  while (true) {
+    // Blink LED
+    printf("Blinking!\r\n");
+    gpio_put(led_pin, true);
+    sleep_ms(1000);
+    gpio_put(led_pin, false);
+    sleep_ms(1000);
+  }
 }
